e8d58f25934c6fab0fed39ffe53569d94b3496a6
Justin Lloyd <justin@imf.la>
2018-08-07 19:28:07 -0700

    Add function skeleton to compare different object types. Change comment on first comparer function make it clear it compares objects of the same type.

--- ChainLinq/ExceptBy.cs
+++ ChainLinq/ExceptBy.cs
@@ -6,9 +6,9 @@ namespace ChainLinq
     public static class Enumerable
     {
         /// <summary>
-        /// This function returns the absolute complement of two sets, i.e. it
-        /// returns all of the items in "source" except for any items that
-        /// also appear in "except."
+        /// This function returns the absolute complement of two sets of
+        /// identical object types, i.e. it returns all of the items in
+        /// "source" except for any items that also appear in "except."
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <typeparam name="TKey"></typeparam>
@@ -17,6 +17,24 @@ namespace ChainLinq
         /// <param name="keyComparer"></param>
         /// <returns></returns>
         public static IEnumerable<T> ExceptBy<T, TKey>(this IEnumerable<T> source, IEnumerable<T> except, Func<T, TKey> keyComparer)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// This function returns the absolute complement of two sets of
+        /// different object types, i.e. it returns all of the items in
+        /// "source" except for any items that also appear in "except."
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="sourceKeyComparer"></param>
+        /// <param name="except"></param>
+        /// <param name="exceptKeyComparer"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> ExceptBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> sourceKeyComparer,
+            IEnumerable<T> except, Func<T, TKey> exceptKeyComparer)
         {
             throw new NotImplementedException();
         }
