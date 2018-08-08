008f1f4efda234bb5d2e9c2c68cd8e57d9076ec7
Justin Lloyd <justin@imf.la>
2018-08-07 20:34:20 -0700

    Add test for complex object type of two identical object types.

--- ChainLinqTests/ExceptByTests.cs
+++ ChainLinqTests/ExceptByTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using ChainLinq;
@@ -40,11 +41,64 @@ namespace ChainLinqTests
             "Justin"
         };
 
+        class Sibling
+        {
+            public string Name;
+            public DateTime Birthdate;
+        }
+
+        private List<Sibling> siblingsComplex = new List<Sibling>
+        {
+            new Sibling(){Name="Kevyn", Birthdate = new DateTime(1940,4,1)},
+            new Sibling(){Name="Gail", Birthdate = new DateTime(1940,6,1)},
+            new Sibling(){Name="Robert", Birthdate = new DateTime(1940,7,1)},
+            new Sibling(){Name="Paul", Birthdate = new DateTime(1950,10,1)},
+            new Sibling(){Name="Christopher", Birthdate = new DateTime(1950,1,1)},
+            new Sibling(){Name="Gareth", Birthdate = new DateTime(1950,3,1)},
+            new Sibling(){Name="Nicholas", Birthdate = new DateTime(1950,6,1)},
+            new Sibling(){Name="Amanda", Birthdate = new DateTime(1960,4,1)},
+            new Sibling(){Name="Catherine", Birthdate = new DateTime(1960,2,1)},
+            new Sibling(){Name="Justin", Birthdate = new DateTime(1960,4,1)},
+        };
+
+        private List<Sibling> brothersComplex = new List<Sibling>
+        {
+            new Sibling(){Name="Kevyn", Birthdate = new DateTime(1940,4,1)},
+            new Sibling(){Name="Robert", Birthdate = new DateTime(1940,7,1)},
+            new Sibling(){Name="Paul", Birthdate = new DateTime(1950,10,1)},
+            new Sibling(){Name="Christopher", Birthdate = new DateTime(1950,1,1)},
+            new Sibling(){Name="Gareth", Birthdate = new DateTime(1950,3,1)},
+            new Sibling(){Name="Nicholas", Birthdate = new DateTime(1950,6,1)},
+            new Sibling(){Name="Justin", Birthdate = new DateTime(1960,4,1)},
+        };
+
+        private List<Sibling> sistersComplex = new List<Sibling>
+        {
+            new Sibling(){Name="Gail", Birthdate = new DateTime(1940,6,1)},
+            new Sibling(){Name="Amanda", Birthdate = new DateTime(1960,4,1)},
+            new Sibling(){Name="Catherine", Birthdate = new DateTime(1960,2,1)},
+        };
+
         [TestMethod]
         public void SiblingsExcludingSistersSimple()
         {
             List<string> result = siblings.ExceptBy(sisters, n => n).ToList();
             CollectionAssert.AreEquivalent(brothers, result);
+        }
+
+        [TestMethod]
+        public void SiblingsExcludingBrothersComplexType()
+        {
+            List<Sibling> result = siblingsComplex.ExceptBy(brothersComplex, n => n.Name).ToList();
+            CollectionAssert.AreEqual(sistersComplex, result, new SiblingComparer());
+        }
+
+        private class SiblingComparer : Comparer<Sibling>
+        {
+            public override int Compare(Sibling x, Sibling y)
+            {
+                return x.Name.CompareTo(y.Name);
+            }
         }
     }
 }
