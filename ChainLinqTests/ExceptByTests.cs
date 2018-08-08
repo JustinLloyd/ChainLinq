fd036f5a8e729471cb161333ca411a6c6d3c93c0
Justin Lloyd <justin@imf.la>
2018-08-07 20:47:57 -0700

    Fix up unit test for different types of objects.

--- ChainLinqTests/ExceptByTests.cs
+++ ChainLinqTests/ExceptByTests.cs
@@ -41,10 +41,34 @@ namespace ChainLinqTests
             "Justin"
         };
 
+        enum Gender
+        {
+            Male,
+            Female,
+        }
+
         class Sibling
         {
             public string Name;
             public DateTime Birthdate;
+            public Gender Gender;
+        }
+
+        class Brother : Sibling
+        {
+            public Brother()
+            {
+                Gender = Gender.Male;
+            }
+        }
+
+        class Sister : Sibling
+        {
+            public Sister()
+            {
+                Gender = Gender.Male;
+            }
+
         }
 
         private List<Sibling> siblingsComplex = new List<Sibling>
@@ -79,6 +103,38 @@ namespace ChainLinqTests
             new Sibling(){Name="Catherine", Birthdate = new DateTime(1960,2,1)},
         };
 
+        private List<Sibling> genderedSiblingsComplex = new List<Sibling>
+        {
+            new Sibling(){Name="Kevyn", Birthdate = new DateTime(1940,4,1), Gender = Gender.Male},
+            new Sibling(){Name="Gail", Birthdate = new DateTime(1940,6,1), Gender = Gender.Female},
+            new Sibling(){Name="Robert", Birthdate = new DateTime(1940,7,1), Gender = Gender.Male},
+            new Sibling(){Name="Paul", Birthdate = new DateTime(1950,10,1), Gender = Gender.Male},
+            new Sibling(){Name="Christopher", Birthdate = new DateTime(1950,1,1), Gender = Gender.Male},
+            new Sibling(){Name="Gareth", Birthdate = new DateTime(1950,3,1), Gender = Gender.Male},
+            new Sibling(){Name="Nicholas", Birthdate = new DateTime(1950,6,1), Gender = Gender.Male},
+            new Sibling(){Name="Amanda", Birthdate = new DateTime(1960,4,1), Gender = Gender.Female},
+            new Sibling(){Name="Catherine", Birthdate = new DateTime(1960,2,1), Gender = Gender.Female},
+            new Sibling(){Name="Justin", Birthdate = new DateTime(1960,4,1), Gender = Gender.Male},
+        };
+
+        private List<Brother> genderedBrothersComplex = new List<Brother>
+        {
+            new Brother(){Name="Kevyn", Birthdate = new DateTime(1940,4,1), Gender = Gender.Male},
+            new Brother(){Name="Robert", Birthdate = new DateTime(1940,7,1), Gender = Gender.Male},
+            new Brother(){Name="Paul", Birthdate = new DateTime(1950,10,1), Gender = Gender.Male},
+            new Brother(){Name="Christopher", Birthdate = new DateTime(1950,1,1), Gender = Gender.Male},
+            new Brother(){Name="Gareth", Birthdate = new DateTime(1950,3,1), Gender = Gender.Male},
+            new Brother(){Name="Nicholas", Birthdate = new DateTime(1950,6,1), Gender = Gender.Male},
+            new Brother(){Name="Justin", Birthdate = new DateTime(1960,4,1), Gender = Gender.Male},
+        };
+
+        private List<Sister> genderedSistersComplex = new List<Sister>
+        {
+            new Sister(){Name="Gail", Birthdate = new DateTime(1940,6,1), Gender = Gender.Female},
+            new Sister(){Name="Amanda", Birthdate = new DateTime(1960,4,1), Gender = Gender.Female},
+            new Sister(){Name="Catherine", Birthdate = new DateTime(1960,2,1), Gender = Gender.Female},
+        };
+
         [TestMethod]
         public void SiblingsExcludingSistersSimple()
         {
@@ -91,6 +147,13 @@ namespace ChainLinqTests
         {
             List<Sibling> result = siblingsComplex.ExceptBy(brothersComplex, n => n.Name).ToList();
             CollectionAssert.AreEqual(sistersComplex, result, new SiblingComparer());
+        }
+
+        [TestMethod]
+        public void GenderedSiblingsExcludingSistersComplexType()
+        {
+            List<Sibling> result = genderedSiblingsComplex.ExceptBy(s => s.Name, genderedBrothersComplex, b=>b.Name).ToList();
+            CollectionAssert.AreEqual(genderedSistersComplex, result, new SiblingComparer());
         }
 
         private class SiblingComparer : Comparer<Sibling>
