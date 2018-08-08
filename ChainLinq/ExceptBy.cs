32ac0ecc5621f16c05c683861e96de258c1f919f
Justin Lloyd <justin@imf.la>
2018-08-07 19:26:09 -0700

    Add description of what the function does.

--- ChainLinq/ExceptBy.cs
+++ ChainLinq/ExceptBy.cs
@@ -5,9 +5,21 @@ namespace ChainLinq
 {
     public static class Enumerable
     {
+        /// <summary>
+        /// This function returns the absolute complement of two sets, i.e. it
+        /// returns all of the items in "source" except for any items that
+        /// also appear in "except."
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="except"></param>
+        /// <param name="keyComparer"></param>
+        /// <returns></returns>
         public static IEnumerable<T> ExceptBy<T, TKey>(this IEnumerable<T> source, IEnumerable<T> except, Func<T, TKey> keyComparer)
         {
             throw new NotImplementedException();
         }
+
     }
 }
