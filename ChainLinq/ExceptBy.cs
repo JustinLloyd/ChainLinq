de0d0bb836f9edac9a57b25005436de6523e9eac
Justin Lloyd <justin@imf.la>
2018-08-07 19:21:11 -0700

    Creating a basic ExceptBy extension for LINQ

--- ChainLinq/ExceptBy.cs
+++ ChainLinq/ExceptBy.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ChainLinq
 {
-    public class Class1
+    public static class Enumerable
     {
+        public static IEnumerable<T> ExceptBy<T, TKey>(this IEnumerable<T> source, IEnumerable<T> except, Func<T, TKey> keyComparer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
